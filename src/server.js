b2919b570192b6f9b6218a0d05a86502ab6376c0
Sushanthbhat23 <sushanthbhat23@gmail.com>
2025-07-27 11:15:16 +0530

    add functionality to serve static files

--- src/server.js
+++ src/server.js
@@ -1,7 +1,23 @@
 import express from 'express'
+import path, {dirname} from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
 const PORT = process.env.PORT || 5000
+
+//command to get the file path from the url in the module
+const __filename = fileURLToPath(import.meta.url)
+//tell the os the directory where the file is present
+const __dirname = dirname(__filename)
+
+//middleware
+//this line of code used to tell the code that the static file is in another directory
+app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 app.listen(PORT, () => {
     console.log(`server has started on ${PORT}`);
